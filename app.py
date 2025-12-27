1147f069f221da5fae0b58b17a1f2edd458a7699
kamlesh-sys <22f3003122@ds.study.iitm.sc.in>
2025-12-27 16:15:50 +0000

    Updates of day2
    The Models and login routes added

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+import config
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+import models
+
+import routes
 
 if __name__ == '__main__':
     app.run(debug=True)
